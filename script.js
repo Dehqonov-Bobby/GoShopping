d7728985760f4d277e34681ae425a53a12f89bdb
= <=>
2024-11-20 16:24:17 +0500

    GoShopping

---
+++ script.js
@@ -0,0 +1,281 @@
+let Back = document.getElementById('Back')
+let Back1 = document.getElementById('Back1')
+let Back2 = document.getElementById('Back2')
+let Back3 = document.getElementById('Back3')
+let Back4 = document.getElementById('Back4')
+let Back5 = document.getElementById('Back5')
+let Back6 = document.getElementById('Back6')
+let Back7 = document.getElementById('Back7')
+let Back8 = document.getElementById('Back8')
+let Back9 = document.getElementById('Back9')
+let Back10 = document.getElementById('Back10')
+let Back11 = document.getElementById('Back11')
+let Back12 = document.getElementById('Back12')
+let Back13 = document.getElementById('Back13')
+let Back14 = document.getElementById('Back14')
+let Back15 = document.getElementById('Back15')
+let Back16 = document.getElementById('Back16')
+let Back17 = document.getElementById('Back17')
+let Back18 = document.getElementById('Back18')
+let Back19 = document.getElementById('Back19')
+let Back20 = document.getElementById('Back20')
+let Back21 = document.getElementById('Back21')
+let Back22 = document.getElementById('Back22')
+let Back23 = document.getElementById('Back23')
+
+
+let btn = document.getElementById('btn')
+let btn1 = document.getElementById('btn1')
+let btn2 = document.getElementById('btn2')
+let btn3 = document.getElementById('btn3')
+let btn4 = document.getElementById('btn4')
+let btn5 = document.getElementById('btn5')
+let btn6 = document.getElementById('btn6')
+let btn7 = document.getElementById('btn7')
+let btn8 = document.getElementById('btn8')
+let btn9 = document.getElementById('btn9')
+let btn10 = document.getElementById('btn10')
+let btn11 = document.getElementById('btn11')
+let btn12 = document.getElementById('btn12')
+let btn13 = document.getElementById('btn13')
+let btn14 = document.getElementById('btn14')
+let btn15 = document.getElementById('btn15')
+let btn16 = document.getElementById('btn16')
+let btn17 = document.getElementById('btn17')
+let btn18 = document.getElementById('btn18')
+let btn19 = document.getElementById('btn19')
+let btn20 = document.getElementById('btn20')
+let btn21 = document.getElementById('btn21')
+let btn22 = document.getElementById('btn22')
+let btn23 = document.getElementById('btn23')
+
+
+Back.addEventListener('click', () => {
+    document.body.querySelector('.About').style.display = 'none'
+    document.body.querySelector('.container').style.display = 'block'
+})
+
+Back1.addEventListener('click', () => {
+    document.body.querySelector('.About1').style.display = 'none'
+    document.body.querySelector('.container').style.display = 'block'
+})
+
+Back2.addEventListener('click', () => {
+    document.body.querySelector('.About2').style.display = 'none'
+    document.body.querySelector('.container').style.display = 'block'
+})
+
+Back3.addEventListener('click', () => {
+    document.body.querySelector('.About3').style.display = 'none'
+    document.body.querySelector('.container').style.display = 'block'
+})
+
+Back4.addEventListener('click', () => {
+    document.body.querySelector('.About4').style.display = 'none'
+    document.body.querySelector('.container').style.display = 'block'
+})
+
+Back5.addEventListener('click', () => {
+    document.body.querySelector('.About5').style.display = 'none'
+    document.body.querySelector('.container').style.display = 'block'
+})
+
+Back6.addEventListener('click', () => {
+    document.body.querySelector('.About6').style.display = 'none'
+    document.body.querySelector('.container').style.display = 'block'
+})
+
+Back7.addEventListener('click', () => {
+    document.body.querySelector('.About7').style.display = 'none'
+    document.body.querySelector('.container').style.display = 'block'
+})
+
+Back8.addEventListener('click', () => {
+    document.body.querySelector('.About8').style.display = 'none'
+    document.body.querySelector('.container').style.display = 'block'
+})
+
+Back9.addEventListener('click', () => {
+    document.body.querySelector('.About9').style.display = 'none'
+    document.body.querySelector('.container').style.display = 'block'
+})
+
+Back10.addEventListener('click', () => {
+    document.body.querySelector('.About10').style.display = 'none'
+    document.body.querySelector('.container').style.display = 'block'
+})
+
+Back11.addEventListener('click', () => {
+    document.body.querySelector('.About11').style.display = 'none'
+    document.body.querySelector('.container').style.display = 'block'
+})
+Back12.addEventListener('click', () => {
+    document.body.querySelector('.About12').style.display = 'none'
+    document.body.querySelector('.container').style.display = 'block'
+})
+Back13.addEventListener('click', () => {
+    document.body.querySelector('.About13').style.display = 'none'
+    document.body.querySelector('.container').style.display = 'block'
+})
+Back14.addEventListener('click', () => {
+    document.body.querySelector('.About14').style.display = 'none'
+    document.body.querySelector('.container').style.display = 'block'
+})
+Back15.addEventListener('click', () => {
+    document.body.querySelector('.About15').style.display = 'none'
+    document.body.querySelector('.container').style.display = 'block'
+})
+Back16.addEventListener('click', () => {
+    document.body.querySelector('.About16').style.display = 'none'
+    document.body.querySelector('.container').style.display = 'block'
+})
+Back17.addEventListener('click', () => {
+    document.body.querySelector('.About17').style.display = 'none'
+    document.body.querySelector('.container').style.display = 'block'
+})
+// Back18.addEventListener('click', () => {
+//     document.body.querySelector('.About18').style.display = 'none'
+//     document.body.querySelector('.container').style.display = 'block'
+// })
+Back19.addEventListener('click', () => {
+    document.body.querySelector('.About19').style.display = 'none'
+    document.body.querySelector('.container').style.display = 'block'
+})
+Back20.addEventListener('click', () => {
+    document.body.querySelector('.About20').style.display = 'none'
+    document.body.querySelector('.container').style.display = 'block'
+})
+Back21.addEventListener('click', () => {
+    document.body.querySelector('.About21').style.display = 'none'
+    document.body.querySelector('.container').style.display = 'block'
+})
+Back22.addEventListener('click', () => {
+    document.body.querySelector('.About22').style.display = 'none'
+    document.body.querySelector('.container').style.display = 'block'
+})
+Back23.addEventListener('click', () => {
+    document.body.querySelector('.About23').style.display = 'none'
+    document.body.querySelector('.container').style.display = 'block'
+})
+
+
+
+btn.addEventListener('click', () => {
+    document.body.querySelector('.About').style.display = 'block'
+    document.body.querySelector('.container').style.display = 'none'
+})
+
+btn1.addEventListener('click', () => {
+    document.body.querySelector('.About1').style.display = 'block'
+    document.body.querySelector('.container').style.display = 'none'
+})
+
+btn2.addEventListener('click', () => {
+    document.body.querySelector('.About2').style.display = 'block'
+    document.body.querySelector('.container').style.display = 'none'
+})
+
+btn3.addEventListener('click', () => {
+    document.body.querySelector('.About3').style.display = 'block'
+    document.body.querySelector('.container').style.display = 'none'
+})
+
+btn4.addEventListener('click', () => {
+    document.body.querySelector('.About4').style.display = 'block'
+    document.body.querySelector('.container').style.display = 'none'
+})
+
+btn5.addEventListener('click', () => {
+    document.body.querySelector('.About5').style.display = 'block'
+    document.body.querySelector('.container').style.display = 'none'
+})
+
+btn6.addEventListener('click', () => {
+    document.body.querySelector('.About6').style.display = 'block'
+    document.body.querySelector('.container').style.display = 'none'
+})
+
+btn7.addEventListener('click', () => {
+    document.body.querySelector('.About7').style.display = 'block'
+    document.body.querySelector('.container').style.display = 'none'
+})
+
+btn8.addEventListener('click', () => {
+    document.body.querySelector('.About8').style.display = 'block'
+    document.body.querySelector('.container').style.display = 'none'
+})
+
+btn9.addEventListener('click', () => {
+    document.body.querySelector('.About9').style.display = 'block'
+    document.body.querySelector('.container').style.display = 'none'
+})
+
+btn10.addEventListener('click', () => {
+    document.body.querySelector('.About10').style.display = 'block'
+    document.body.querySelector('.container').style.display = 'none'
+})
+
+btn11.addEventListener('click', () => {
+    document.body.querySelector('.About11').style.display = 'block'
+    document.body.querySelector('.container').style.display = 'none'
+})
+
+btn12.addEventListener('click', () => {
+    document.body.querySelector('.About12').style.display = 'block'
+    document.body.querySelector('.container').style.display = 'none'
+})
+
+btn13.addEventListener('click', () => {
+    document.body.querySelector('.About13').style.display = 'block'
+    document.body.querySelector('.container').style.display = 'none'
+})
+
+btn14.addEventListener('click', () => {
+    document.body.querySelector('.About14').style.display = 'block'
+    document.body.querySelector('.container').style.display = 'none'
+})
+
+btn15.addEventListener('click', () => {
+    document.body.querySelector('.About15').style.display = 'block'
+    document.body.querySelector('.container').style.display = 'none'
+})
+
+btn16.addEventListener('click', () => {
+    document.body.querySelector('.About16').style.display = 'block'
+    document.body.querySelector('.container').style.display = 'none'
+})
+
+btn17.addEventListener('click', () => {
+    document.body.querySelector('.About17').style.display = 'block'
+    document.body.querySelector('.container').style.display = 'none'
+})
+
+btn18.addEventListener('click', () => {
+    document.body.querySelector('.About18').style.display = 'block'
+    document.body.querySelector('.container').style.display = 'none'
+})
+
+btn19.addEventListener('click', () => {
+    document.body.querySelector('.About19').style.display = 'block'
+    document.body.querySelector('.container').style.display = 'none'
+})
+
+btn20.addEventListener('click', () => {
+    document.body.querySelector('.About20').style.display = 'block'
+    document.body.querySelector('.container').style.display = 'none'
+})
+
+btn21.addEventListener('click', () => {
+    document.body.querySelector('.About21').style.display = 'block'
+    document.body.querySelector('.container').style.display = 'none'
+})
+
+btn22.addEventListener('click', () => {
+    document.body.querySelector('.About22').style.display = 'block'
+    document.body.querySelector('.container').style.display = 'none'
+})
+
+btn23.addEventListener('click', () => {
+    document.body.querySelector('.About23').style.display = 'block'
+    document.body.querySelector('.container').style.display = 'none'
+})
